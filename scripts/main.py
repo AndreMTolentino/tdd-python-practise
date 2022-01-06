5857b74f80d2d31535058ec0e705ffaba5e7c2cc
AndreMTolentino <andre.m.tolentino@gmail.com>
2022-01-06 13:00:57 -0300

    Second Lesson:Adding fractions with same denominator

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,17 @@
 class Fraction:
 
-    def __init__(self, intergerValue):
-        self.intergerValue = intergerValue
+    def __init__(self, numerator, denominator=1):
+        self.numerator = numerator
+        self.denominator = denominator
 
-    def add(self, numb):
-        return Fraction(self.intergerValue + numb)
+    def add(self, Number):
+        return Fraction(self.numerator + Number.numerator, self.denominator)
 
     def intValue(self):
-        return self.intergerValue
+        return self.numerator
+
+    def Numerator(self):
+        return self.numerator
+
+    def Denominator(self):
+        return self.denominator
--- scripts/tests/test_unit.py
+++ scripts/tests/test_unit.py
@@ -2,25 +2,31 @@ from scripts.main import Fraction
 
 
 def test_zero_plus_zero():
-    result = Fraction(0).add(0)
+    result = Fraction(0).add(Fraction(0))
     assert result.intValue() == 0
 
 
 def test_zero_plus_number():
-    result = Fraction(0).add(2)
+    result = Fraction(0).add(Fraction(2))
     assert result.intValue() == 2
 
 
 def test_number_plus_zero():
-    result = Fraction(3).add(0)
+    result = Fraction(3).add(Fraction(0))
     assert result.intValue() == 3
 
 
 def test_number_plus_number():
-    result = Fraction(1).add(6)
+    result = Fraction(1).add(Fraction(6))
     assert result.intValue() == 7
 
 
 def test_negative_number_plus_number():
-    result = Fraction(-3).add(1)
+    result = Fraction(-3).add(Fraction(1))
     assert result.intValue() == -2
+
+
+def test_sum_non_trivial_common_denominator():
+    result = Fraction(1, 5).add(Fraction(2, 5))
+    assert 3 == result.Numerator()
+    assert 5 == result.Denominator()
